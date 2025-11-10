1c56ba7081d94c34fbfc2bf0c4e4f7e6d93f31fa
Muhammad Ibnu Atho'illah <150983149+Rei911@users.noreply.github.com>
2025-11-10 18:38:08 +0700

    Update Main.ino

--- 07_RotaryEncoder/Main.ino
+++ 07_RotaryEncoder/Main.ino
@@ -5,11 +5,10 @@
 // Encoder Pins
 #define CLK 18
 #define DT  19
-#define SW  21
 
 // Task Handles
-TaskHandle_t HandleTaskEncoderCore0;
-TaskHandle_t HandleTaskButtonCore1;
+TaskHandle_t TaskEncoderCore0;
+TaskHandle_t TaskEncoderCore1;
 
 volatile int encoderValue = 0;
 int lastCLKState;
@@ -25,23 +24,19 @@ void IRAM_ATTR readEncoder() {
   lastCLKState = currentCLK;
 }
 
-// Task di Core 0 → Monitor nilai encoder
-void TaskEncoderCore0(void *pvParameters) {
+// Task di Core 0
+void Task_ReadEncoder_Core0(void *pvParameters) {
   for(;;){
-    Serial.printf("[Core %d] Encoder Value: %d\n",
-                  xPortGetCoreID(), encoderValue);
-    vTaskDelay(200 / portTICK_PERIOD_MS);
+    Serial.printf("[CORE 0] Encoder Value: %d\n", encoderValue);
+    vTaskDelay(pdMS_TO_TICKS(200));
   }
 }
 
-// Task di Core 1 → Deteksi tombol SW
-void TaskButtonCore1(void *pvParameters) {
+// Task di Core 1
+void Task_ReadEncoder_Core1(void *pvParameters) {
   for(;;){
-    if (digitalRead(SW) == LOW) {
-      Serial.printf("[Core %d] BUTTON PRESSED\n", xPortGetCoreID());
-      vTaskDelay(300 / portTICK_PERIOD_MS); // debounce
-    }
-    vTaskDelay(1);
+    Serial.printf("[CORE 1] Encoder Value: %d\n", encoderValue);
+    vTaskDelay(pdMS_TO_TICKS(700));
   }
 }
 
@@ -50,35 +45,15 @@ void setup() {
 
   pinMode(CLK, INPUT_PULLUP);
   pinMode(DT, INPUT_PULLUP);
-  pinMode(SW, INPUT_PULLUP);
 
   lastCLKState = digitalRead(CLK);
 
   attachInterrupt(digitalPinToInterrupt(CLK), readEncoder, CHANGE);
 
-  xTaskCreatePinnedToCore(
-    TaskEncoderCore0,
-    "EncoderCore0",
-    2048,
-    NULL,
-    1,
-    &HandleTaskEncoderCore0,
-    0
-  );
+  xTaskCreatePinnedToCore(Task_ReadEncoder_Core0, "EncoderCore0", 2048, NULL, 1, &TaskEncoderCore0, 0);
+  xTaskCreatePinnedToCore(Task_ReadEncoder_Core1, "EncoderCore1", 2048, NULL, 1, &TaskEncoderCore1, 1);
 
-  xTaskCreatePinnedToCore(
-    TaskButtonCore1,
-    "ButtonCore1",
-    2048,
-    NULL,
-    1,
-    &HandleTaskButtonCore1,
-    1
-  );
-
-  Serial.println("Dual-Core Encoder Ready!");
+  Serial.println("Dual-Core Encoder Read Started");
 }
 
-void loop() {
-  // kosong
-}
+void loop() {}
